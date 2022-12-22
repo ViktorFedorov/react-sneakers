7ccad6b7335fa31c39439f51bacebb2e48e0b6c8
ViktorF <logforpractik@yandex.ru>
2022-12-22 17:31:35 +0300

    input search

--- src/components/goods-list/goods-list.js
+++ src/components/goods-list/goods-list.js
@@ -5,7 +5,13 @@ import GoodsItem from '../goods-item/goods-item'
 const GoodsList = () => {
   return (
     <div className={styles.goods}>
-      <h1>Все кроссовки</h1>
+      <div className={styles.goodsListHeading}>
+        <h1>Все кроссовки</h1>
+        <input
+          className={styles.searchBar}
+          placeholder='Поиск...'
+          type="text"/>
+      </div>
       <ul className={styles.goodsList}>
         <GoodsItem />
         <GoodsItem />
